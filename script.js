3b42434e36c9abea4b5c1da80d21b5db16bc1d2b
drahmedgabr <ahmadal4ever@gmail.com>
2025-01-21 17:42:00 +0200

    finish mobile ui

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const qrInput = document.getElementById("qrInput");
 const canvas = document.getElementById("canvas");
+const qrParagraph = document.getElementById("qrParagraph");
+const canvasDiv = document.getElementById("canvasDiv");
 
 function generateCode() {
     const qrText = qrInput.value;
-    if(qrText == ""){
+    if(qrText === ""){
         alert("Please enter your text");
     } else {
+
         var qr = new QRious({
             element: canvas,
             value: qrText,
-          });
+            foreground: "#850685",
+            size: 140,
+        });
+
+        qrParagraph.innerText = qrText;
+        qrInput.value = "";
+        canvasDiv.style.display = "block";
     }
 }
